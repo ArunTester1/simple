88f7065fd797adb40cabc122bc6dd5697cd9d123
ArunTester1 <aravinthcoder@gmail.com>
2019-12-24 11:21:00 +0530

    Tocheck

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,26 @@
+package org.lang;
+
+public class LanguageInfo
+{
+public void tamillanguage()
+{
+	System.out.println("Tamil language");
+}
+public void Englishlanguage()
+{
+	System.out.println("English language");
+}
+public void Hindilanguage()
+{
+	System.out.println("Hindi language");
+}
+
+public static void main(String[] args) 
+{
+LanguageInfo LI = new LanguageInfo();
+LI.tamillanguage();
+LI.Englishlanguage();
+
+}
+}
+
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,23 @@
+package org.lang;
+
+public class StateDetails extends LanguageInfo
+{
+	public void SouthIndia()
+	{
+		System.out.println("This Is SouthIndia");
+	}
+	public void NorthIndia()
+	{
+		System.out.println("This Is NorthIndia");
+	}
+
+public static void main(String[] args) {
+	StateDetails SD = new StateDetails();
+	SD.SouthIndia();
+	SD.NorthIndia();
+	SD.tamillanguage();
+	SD.Englishlanguage();
+	
+}
+}
+
